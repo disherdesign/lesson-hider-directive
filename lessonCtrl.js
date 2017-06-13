08529ac6368e37a196b22bb8f9bec2fabd38a5fb
disherdesign <drewdisher@fastmail.com>
2017-06-13 14:47:03 -0500

    undefined alert

---
+++ lessonCtrl.js
@@ -0,0 +1,13 @@
+angular.module('directivePractice')
+.controller('lessonCtrl', function ($scope){
+
+	$scope.lessons = lessons;
+
+	$scope.announceDay = function(lesson, day) { 
+		alert(lesson + " is active on " + day +  '.')
+	
+};
+	
+});
+
+	var lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
